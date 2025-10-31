82a33299a7c5a5daad593a1fc79f52c0ad83856f
ivazanc@gitlab.inf.uva.es <ivan.zancajo22@estudiantes.uva.es>
2025-10-31 20:29:48 +0100

    feat: crea fichero conexiones/sesiones de sqlalchemy

---
+++ backend/db.py
@@ -0,0 +1,31 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+if DATABASE_URL is None:
+    raise ValueError("No se ha definido la variable de entorno DATABASE_URL")
+
+class Base(DeclarativeBase):
+    pass
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, future=True)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
+
+# def init_db():
+#     Base.metadata.create_all(bind=engine)
+# Inicializar BBDD
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
