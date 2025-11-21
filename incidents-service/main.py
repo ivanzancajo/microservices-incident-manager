b10fcc3f7d7f66369d50b29f50562ad8487a4898
danifer <daniel.fernandez22@estudidantes.uva.es>
2025-11-21 19:39:17 +0100

    feat: implementa endpoint  GET /incidencias/{id}

---
+++ incidents-service/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Depends, Query
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+
+from db import Base, engine, get_db
+import models, schemas, crud
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI(title="Servicio de Usuarios con Persistencia")
+
+# Middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/incidencias/{incident_id}", response_model=schemas.IncidentOut)
+def get_incident_endpoint( incident_id:int, db: Session = Depends(get_db)):
+    return crud.get_incident(db, incident_id)
+
+
+
+
+
+
