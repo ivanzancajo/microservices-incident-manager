b6a3a902a4025e6b01b8214207a6310bf21bdc60
ivazanc@gitlab.inf.uva.es <ivan.zancajo22@estudiantes.uva.es>
2025-12-13 12:33:26 +0100

    feat: añade lógica de generación de tokens

--- users-service/app/security.py
+++ users-service/app/security.py
@@ -1,20 +1,38 @@
+import os
+from datetime import datetime, timedelta
+from typing import Union
 from passlib.context import CryptContext
+from jose import jwt
 
-# Configuramos bcrypt como el algoritmo de hashing principal
-# "deprecated='auto'" permite que si en el futuro bcrypt se actualiza, 
-# el sistema pueda leer hashes viejos pero cree los nuevos con la versión segura.
+# 1. Configuración de Hashing (lo que ya tenías)
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# 2. Configuración JWT (Leemos las variables que pusimos en el .env)
+SECRET_KEY = os.getenv("JWT_SECRET")
+ALGORITHM = os.getenv("JWT_ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
 def verify_password(plain_password, hashed_password):
-    """
-    Comprueba si la contraseña en texto plano coincide con el hash guardado.
-    Se usará en el Login.
-    """
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password):
-    """
-    Genera un hash seguro a partir de una contraseña en texto plano.
-    Se usará en el Registro.
-    """
     return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    """
+    Genera un JWT firmado con nuestra clave secreta.
+    """
+    to_encode = data.copy()
+    
+    # Calculamos cuándo expira
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    # Añadimos la expiración al payload del token
+    to_encode.update({"exp": expire})
+    
+    # Firmamos el token
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
